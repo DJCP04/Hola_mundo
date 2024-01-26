85db49f911b47e655cb1dcf60a27cb03a8e47928
Denis Chiriboga <dcp00@iesemilidarder.com>
2024-01-26 13:17:26 +0100

    Subimos el proyecto Hola_mundo

---
+++ Hola_Mundo.java
@@ -0,0 +1,17 @@
+package hola_mundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DJ2004
+ */
+public class Hola_Mundo {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Hola mundo");
+    }
+    
+}
